58c63d4cdbac187e13241f322baa3f1e544c2f82
Selva Sabapathy <selva.sabapathy@gmail.com>
2021-05-08 12:57:43 -0400

    Add Invoice create (one and multiple)

--- src/test/java/com/sms/invoicify/invoice/InvoiceControllerIT.java
+++ src/test/java/com/sms/invoicify/invoice/InvoiceControllerIT.java
@@ -1,6 +1,5 @@
 package com.sms.invoicify.invoice;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +8,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -18,20 +18,35 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureRestDocs
 public class InvoiceControllerIT {
 
-    @Autowired
-    private MockMvc mockMvc;
+  @Autowired private MockMvc mockMvc;
 
-    @Autowired
-    private ObjectMapper objectMapper;
+  @Autowired private ObjectMapper objectMapper;
 
-    @Test
-    public void create() throws Exception {
-        InvoiceDto invoice = new InvoiceDto();
-        mockMvc.perform(post("/invoices")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(invoice))
-        )
-                .andExpect(status().isCreated());
-    }
+  private MvcResult create(InvoiceDto invoiceDto) throws Exception {
+    MvcResult mvcResult =
+        mockMvc
+            .perform(
+                post("/invoices")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(objectMapper.writeValueAsString(invoiceDto)))
+            .andExpect(status().isCreated())
+            .andReturn();
 
+    return mvcResult;
+  }
+
+  @Test
+  public void createOne() throws Exception {
+    InvoiceDto invoiceDto = new InvoiceDto();
+    create(invoiceDto);
+  }
+
+  @Test
+  public void createMultiple() throws Exception {
+    InvoiceDto invoiceDto1 = new InvoiceDto();
+    create(invoiceDto1);
+
+    InvoiceDto invoiceDto2 = new InvoiceDto();
+    create(invoiceDto2);
+  }
 }
