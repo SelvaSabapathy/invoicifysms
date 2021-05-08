64c776c1f7ac5149ce8164ccfaba65ee97524719
j-tanz <john.tansor@cognizant.com>
2021-05-08 16:18:06 -0400

    updates ItemService with implementation for get All items

--- src/main/java/com/sms/invoicify/service/ItemService.java
+++ src/main/java/com/sms/invoicify/service/ItemService.java
@@ -6,17 +6,36 @@ import com.sms.invoicify.repository.ItemsRepositiory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 @RequiredArgsConstructor
 public class ItemService {
 
   private final ItemsRepositiory itemsRepositiory;
 
-  public void createItem(Item itemDto) {
-    itemsRepositiory.save(ItemEntity.builder()
-            .description(itemDto.getDescription())
-            .quantity(itemDto.getQuantity())
-            .totalFees(itemDto.getTotalFees())
-            .build());
+  public Long createItem(Item itemDto) {
+    ItemEntity persisted =
+        itemsRepositiory.save(
+            ItemEntity.builder()
+                .description(itemDto.getDescription())
+                .quantity(itemDto.getQuantity())
+                .totalFees(itemDto.getTotalFees())
+                .build());
+    return persisted.getItemId();
+  }
+
+  public List<Item> fetchAllItems() {
+    return itemsRepositiory.findAll().stream()
+        .map(
+            itemEntity -> {
+              return Item.builder()
+                  .description(itemEntity.getDescription())
+                  .quantity(itemEntity.getQuantity())
+                  .totalFees(itemEntity.getTotalFees())
+                  .build();
+            })
+        .collect(Collectors.toList());
   }
 }
--- src/test/java/com/sms/invoicify/service/ItemServiceTest.java
+++ src/test/java/com/sms/invoicify/service/ItemServiceTest.java
@@ -11,37 +11,79 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.math.BigDecimal;
+import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class ItemServiceTest {
 
-    @Mock
-    ItemsRepositiory itemsRepositiory;
+  @Mock ItemsRepositiory itemsRepositiory;
 
-    @InjectMocks
-    ItemService itemService;
+  @InjectMocks ItemService itemService;
 
   @Test
   @DisplayName("Create new Item Test")
   void createItem() {
-      Item itemDto = Item.builder()
-              .description("MyInvoiceItem")
-              .quantity(42)
-              .totalFees(BigDecimal.valueOf(10.99))
-              .build();
+    Item itemDto =
+        Item.builder()
+            .description("MyInvoiceItem")
+            .quantity(42)
+            .totalFees(BigDecimal.valueOf(10.99))
+            .build();
 
-    itemService.createItem(itemDto);
+    ItemEntity itemEntity =
+        ItemEntity.builder()
+            .description("MyInvoiceItem")
+            .quantity(42)
+            .totalFees(BigDecimal.valueOf(10.99))
+            .build();
 
-   verify(itemsRepositiory).save(
-           ItemEntity.builder()
-                   .description("MyInvoiceItem")
-                   .quantity(42)
-                   .totalFees(BigDecimal.valueOf(10.99))
-                   .build()
-   );
-   verifyNoMoreInteractions(itemsRepositiory);
+    when(itemsRepositiory.save(any()))
+        .thenReturn(
+            ItemEntity.builder()
+                .itemId(8L)
+                .description("MyInvoiceItem")
+                .quantity(42)
+                .totalFees(BigDecimal.valueOf(10.99))
+                .build());
+
+    Long itemId = itemService.createItem(itemDto);
+    verify(itemsRepositiory).save(itemEntity);
+    verifyNoMoreInteractions(itemsRepositiory);
+    assertEquals(8L, itemId);
+  }
+
+  @Test
+  @DisplayName("Get All Items")
+  void getAllItems() {
+    when(itemsRepositiory.findAll())
+        .thenReturn(
+            List.of(
+                ItemEntity.builder()
+                    .itemId(8L)
+                    .description("MyInvoiceItem")
+                    .quantity(42)
+                    .totalFees(BigDecimal.valueOf(10.99))
+                    .build()));
+
+    List<Item> itemsFromService = itemService.fetchAllItems();
+
+    Item itemDtoExpected =
+        Item.builder()
+            .description("MyInvoiceItem")
+            .quantity(42)
+            .totalFees(BigDecimal.valueOf(10.99))
+            .build();
+
+    assertThat(itemsFromService).isEqualTo(List.of(itemDtoExpected));
+
+    verify(itemsRepositiory).findAll();
+    verifyNoMoreInteractions(itemsRepositiory);
   }
 }
