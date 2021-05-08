19a875390c97ce9d15b6c577e3d6f50be1aa2bfa
maanick <maanickavasagam@gmail.com>
2021-05-08 12:25:13 -0600

    Green Test for Multiple post and get all items

--- src/test/java/com/sms/invoicify/ItemIT.java
+++ src/test/java/com/sms/invoicify/ItemIT.java
@@ -13,6 +13,8 @@ import org.springframework.test.web.servlet.MockMvc;
 import java.math.BigDecimal;
 
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -53,6 +55,41 @@ public class ItemIT {
                 .andExpect(jsonPath("[0].quantity").value(1))
                 .andExpect(jsonPath("[0].totalFees").value(10.00));
 
+    }
+
+    @Test
+    void postMultipleItem() throws Exception{
+        Item item =  Item.builder().description("Test Item Description").quantity(1).totalFees(BigDecimal.TEN).build();
+        Item item1 =  Item.builder().description("Test Item Description2").quantity(2).totalFees(BigDecimal.valueOf(5)).build();
+        mockMvc.perform(post("/items").contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(item)))
+                .andExpect(status().isCreated())
+                .andExpect(content().string("Test Item Description created Successfully"))
+                ;
+
+        mockMvc.perform(post("/items").contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(item1)))
+                .andExpect(status().isCreated())
+                .andExpect(content().string("Test Item Description2 created Successfully"))
+                ;
+
+    mockMvc
+        .perform(get("/items"))
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("length()").value(2))
+        .andExpect(jsonPath("[0].description").value("Test Item Description"))
+        .andExpect(jsonPath("[0].quantity").value(1))
+        .andExpect(jsonPath("[0].totalFees").value(10.00))
+        .andExpect(jsonPath("[1].description").value("Test Item Description2"))
+        .andExpect(jsonPath("[1].quantity").value(2))
+        .andExpect(jsonPath("[1].totalFees").value(5.00))
+        .andDo(document("GetAllItems", responseFields(
+                fieldWithPath("[0].description").description("Test Item Description"),
+                fieldWithPath("[0].quantity").description(1),
+                fieldWithPath("[0].totalFees").description(10.00),
+                fieldWithPath("[1].description").description("Test Item Description2"),
+                fieldWithPath("[1].quantity").description(2),
+                fieldWithPath("[1].totalFees").description(5))));
     }
 
 
