a51aa0ab7cda5c1aed5273d179e4e13fbf6ee906
maanick <maanickavasagam@gmail.com>
2021-05-08 12:13:16 -0600

    Green Test for Post Item and Get Item

--- src/main/java/com/sms/invoicify/controller/ItemsController.java
+++ src/main/java/com/sms/invoicify/controller/ItemsController.java
@@ -9,23 +9,26 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping("/items")
 public class ItemsController {
 
+    List<Item> itemList = new ArrayList<>();
 
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
-    public List<String> getAllItems(){
-        return List.of();
+    public List<Item> getAllItems(){
+        return itemList;
 
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public String postItem(@RequestBody Item item){
+        itemList.add(item);
         return item.getDescription() + " created Successfully";
 
     }
